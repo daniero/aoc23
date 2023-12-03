0e17552c623d458483a9fe2f2e11269112f8e2fd
Daniel Rødskog <danielmero@gmail.com>
2023-12-03 17:54:45 +0100

    Refactor part 1

--- src/solutions/03/Day3.tsx
+++ src/solutions/03/Day3.tsx
@@ -3,12 +3,13 @@ import { type ReactElement } from 'react';
 import { DayTitle } from '../../components/DayTitle.tsx';
 import { InputSelector, useInput } from '../../components/InputSelector.tsx';
 import input1 from './input1.txt?raw';
-import { range } from '../../utils/arrays.ts';
+import { loop, range } from '../../utils/arrays.ts';
 
 export default function Day2(): ReactElement {
   const input = useInput([['Sample 1', input1]]);
 
-  const part1 = solvePart1(input.value);
+  const grid = createGrid(input.value);
+  const part1 = solvePart1(grid);
 
   return (
     <Container size={'sm'}>
@@ -17,35 +18,82 @@ export default function Day2(): ReactElement {
       <InputSelector input={input} />
 
       <h2>Solution</h2>
-      <h3>Parsed data</h3>
-      <Card>
-        <pre>{JSON.stringify(part1, undefined, 2)}</pre>
-      </Card>
+      <h3>Part 1</h3>
+      <Card>{part1}</Card>
     </Container>
   );
 }
 
-function solvePart1(input: string): any {
+/**
+ * [x, y, element]
+ */
+type Neighbour<T> = [number, number, T];
+
+function neighbours<T>(grid: T[][], x: number, y: number): Array<Neighbour<T>> {
+  const yStart = Math.max(0, y - 1);
+  const yStop = Math.min(grid.length, y + 2);
+
+  return range(yStart, yStop).flatMap((ny) => {
+    const xStart = Math.max(0, x - 1);
+    const xStop = Math.min(grid[ny].length, x + 2);
+
+    return range(xStart, xStop)
+      .filter((nx) => !(nx === x && ny === y))
+      .map((nx) => [nx, ny, grid[ny][nx]] as Neighbour<T>);
+  });
+}
+
+interface Node {
+  x: number;
+  y: number;
+  char: string;
+  id: symbol | null;
+  n: number | null;
+}
+
+export function createGrid(input: string): Node[][] {
   const lines = input.trim().split('\n');
-  let sum = 0;
+
+  const grid = lines.map((line, y) => {
+    const trimmed = line.trimEnd();
+    return loop(trimmed.length).map((x) => ({
+      id: null as symbol | null,
+      x,
+      y,
+      char: trimmed.charAt(x),
+      n: null as number | null,
+    }));
+  });
 
   lines.forEach((line, y) => {
     match(line, /\d+/g).forEach(({ index: x, 0: s }) => {
-      const isPartNumber = range(y - 1, y + 2).some((ny) => {
-        const inYRange = ny >= 0 && ny < lines.length;
-        return (
-          inYRange &&
-          range(x - 1, x + s.length + 1).some((nx) => {
-            const inXrange = nx >= 0 && nx < line.length;
-            if (!inXrange) return false;
-            const char = lines[ny].charAt(nx);
-            return char !== '.' && !/\d/.test(char);
-          })
-        );
-      });
+      const n = parseInt(s);
+      const id = Symbol(n);
 
-      if (isPartNumber) {
-        sum += parseInt(s);
+      loop(s.length, x).forEach((x) => {
+        grid[y][x].n = n;
+        grid[y][x].id = id;
+      });
+    });
+  });
+
+  return grid;
+}
+
+export function solvePart1(grid: Node[][]): number {
+  let sum = 0;
+  const visitedNumbers = new Set<symbol>();
+
+  grid.forEach((row) => {
+    row.forEach((e) => {
+      if (e.id !== null && !visitedNumbers.has(e.id)) {
+        for (const [, , n] of neighbours(grid, e.x, e.y)) {
+          if (n.char !== '.' && !/\d/.test(n.char)) {
+            sum += e.n as number;
+            visitedNumbers.add(e.id);
+            break;
+          }
+        }
       }
     });
   });
--- src/utils/arrays.ts
+++ src/utils/arrays.ts
@@ -3,6 +3,10 @@ export function range(start: number, stop: number): number[] {
   return new Array(size).fill(false).map((_, i) => i + start);
 }
 
+export function loop(length: number, start: number = 0): number[] {
+  return new Array(length).fill(false).map((_, i) => i + start);
+}
+
 export function fill<T>(size: number, fn: (i: number) => T): T[] {
   return new Array(size).fill(false).map((_, i) => fn(i));
 }
