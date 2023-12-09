f6476c851815689d90914041487e95256434d4a3
Daniel Rødskog <danielmero@gmail.com>
2023-12-09 14:21:07 +0100

    Solve day 8 part 2

--- src/solutions/08/Day8.tsx
+++ src/solutions/08/Day8.tsx
@@ -5,7 +5,7 @@ import input2 from './input2.txt?raw';
 import { InputSelector, useInput } from '../../components/InputSelector.tsx';
 import { DayTitle } from '../../components/DayTitle.tsx';
 import { match } from '../../utils/regex.ts';
-import { loop } from '../../utils/arrays.ts';
+import { lcm } from '../../utils/math.ts';
 
 export default function Day8(): ReactElement {
   const input = useInput([
@@ -15,7 +15,7 @@ export default function Day8(): ReactElement {
 
   const parsedInput = parseInput(input.value);
   const part1 = solvePart1(parsedInput);
-  // const part2 = solvePart2(parsedInput);
+  const part2 = solvePart2(parsedInput);
 
   return (
     <>
@@ -25,52 +25,85 @@ export default function Day8(): ReactElement {
         <h2>Input</h2>
         <InputSelector input={input} />
 
-        {/* <h3>Parsed</h3> */}
-        {/* <Card component={'pre'}> */}
-        {/*  {JSON.stringify(parsedInput, undefined, 2)} */}
-        {/* </Card> */}
+        <h3>Parsed</h3>
+        <Card component={'pre'}>
+          {JSON.stringify(parsedInput, undefined, 2)}
+        </Card>
 
         <h2>Solution</h2>
 
         <h3>Part 1</h3>
         <Card component={'pre'}>{JSON.stringify(part1, undefined, 2)}</Card>
 
-        {/* <h3>Part 2</h3> */}
-        {/* <Card component={'pre'}>{JSON.stringify(part2, undefined, 2)}</Card> */}
+        <h3>Part 2</h3>
+        <Card component={'pre'}>{JSON.stringify(part2, undefined, 2)}</Card>
       </Container>
     </>
   );
 }
 
-type Foo = ReturnType<typeof parseInput>;
+interface ParsedInput {
+  steps: Array<'L' | 'R'>;
+  nodes: Record<
+    string,
+    {
+      L: string;
+      R: string;
+    }
+  >;
+}
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export function parseInput(input: string) {
-  const [steps, n] = input.trim().split('\n\n');
+export function parseInput(input: string): ParsedInput {
+  const [s, n] = input.trim().split('\n\n');
+
+  const steps = s.split('') as ParsedInput['steps'];
 
   const nodes = n
     .split('\n')
-    .map((line) => match(line, /[A-Z]+/g).flat())
+    .map((line) => match(line, /\w+/g).flat())
     .reduce((map, [a, b, c]) => ({ ...map, [a]: { L: b, R: c } }), {});
 
   return { steps, nodes };
 }
 
-export function solvePart1(input: Foo): any {
+export function solvePart1(input: ParsedInput): any {
   const { steps, nodes } = input;
 
-  const looped = loop(100)
-    .map(() => steps)
-    .join('');
-
-  return [...looped].reduce((current, next, i) => {
-    const x = nodes[current][next];
-    // console.log({ i, current, next, x });
-    if (x === 'ZZZ') console.log({ i: i + 1, current, next, x });
-    return x;
-  }, 'AAA');
+  let i = 0;
+  let current = 'AAA';
+  while (true) {
+    const nextStep = steps[i++ % steps.length];
+    const next = nodes[current][nextStep];
+    if (next === 'ZZZ') return i;
+    current = next;
+  }
 }
 
-export function solvePart2(lines: Foo): any {
-  return lines;
+export function solvePart2(input: ParsedInput): any {
+  const { steps, nodes } = input;
+
+  const startNodes = Object.keys(nodes).filter((k) => k.endsWith('A'));
+
+  return startNodes
+    .map((node) => {
+      let i = 0;
+      const zs = [];
+      let currentNode = node;
+      const visited = new Set();
+
+      while (true) {
+        const stepNr = i % steps.length;
+        const nextStep = steps[stepNr];
+
+        currentNode = nodes[currentNode][nextStep];
+        const hash = `${stepNr}-${currentNode}-${nextStep}`;
+        if (visited.has(hash)) return zs;
+        visited.add(hash);
+
+        ++i;
+        if (currentNode.endsWith('Z')) zs.push(i);
+      }
+    })
+    .flat()
+    .reduce(lcm);
 }
